7f7d7c2d54ce2b436252075ae3f8b4b2368df192
Chintan Bawa <chintanbawa@gmail.com>
2023-05-01 02:27:04 +0530

    Setup express and mongoose

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// express
+const app = express();
+var corsOptions = {
+  origin: 'http://localhost:8081',
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// db
+const db = require('./models');
+const dbConfig = require('./config/db.config');
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Successfully connect to MongoDB.');
+  })
+  .catch((err) => {
+    console.error('Connection error', err);
+    process.exit();
+  });
+
+// test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome All.' });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
